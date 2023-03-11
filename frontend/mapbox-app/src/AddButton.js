1e807454eedfcd3f6cbc15704ebb21fb1b5dcca0
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2023-03-11 13:51:17 -0500

    current progress on button

--- frontend/mapbox-app/src/AddButton.js
+++ frontend/mapbox-app/src/AddButton.js
@@ -1,11 +1,44 @@
 import MaterialIcon, {colorPalette} from 'material-icons-react';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import React, { useState } from 'react';
+
+// export default function AddButton() {
+
+//     return (
+//     <Button className="AddButton" variant="primary" onClick={}>
+//         <MaterialIcon icon="add" color="black"></MaterialIcon>
+//     </Button>
+//     );
+    
+// }
 
 export default function AddButton() {
-
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
     return (
-    <Button className="AddButton" variant="primary">
+    <div>
+    <Button className="AddButton" variant="primary" onClick={handleShow}>
         <MaterialIcon icon="add" color="black"></MaterialIcon>
     </Button>
+  
+        <Modal show={show} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+    </div>
     );
-}
+  }
