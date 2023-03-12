178daf59b2c628277031e3e823ca06a2a8304fc2
Samuel <samueltan97@hotmail.com>
2023-03-12 08:58:20 -0400

    Set up firebase but seems to be a bug

--- frontend/mapbox-app/src/uploadButton.js
+++ frontend/mapbox-app/src/uploadButton.js
@@ -1,17 +1,60 @@
 import React, { useRef, useState } from "react";
 import { saveAs } from 'file-saver'
+import { storage } from "firebaseConfig";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
 
 function UploadButton({setImageSource}) {
     const [file, setFile] = useState();
+    // State to store uploaded file
+    const [uploadFile, setUploadFile] = useState("");
+ 
+    // progress
+    const [percent, setPercent] = useState(0);
 
     function handleChange(e) {
         setFile(URL.createObjectURL(e.target.files[0]));
         setImageSource(e.target.files[0])
+        setUploadFile(e.target.files[0])
     }
+
+    const handleUpload = () => {
+      if (!file) {
+        alert("Please upload an image first!");
+      }
+
+      const storageRef = ref(storage, `/files/${file.name}`);
+
+      // progress can be paused and resumed. It also exposes progress updates.
+      // Receives the storage reference and the file to upload.
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const percent = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+
+        // update progress
+        setPercent(percent);
+        },
+        (err) => console.log(err),
+          () => {
+          // download url
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            console.log(url);
+          });
+          }
+      );
+    };
+
     return (
         <div>
         <form action="/action_page.php">
-            <input type="file" id="myFile" name="filename" onChange={handleChange}/>
+            <input type="file" id="myFile" name="filename" onChange={handleChange} accept="/image/*"/>
+            <button onClick={handleUpload}>Upload to Firebase</button>
+            <p>{percent} "% done"</p>
         </form>
         </div>
     );
